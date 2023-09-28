ab2ab7ad137b651c7ed87c28418abcc5179190f4
Igor Augusto Costa Sales <142929802+IgorSales95@users.noreply.github.com>
2023-09-28 15:17:54 -0300

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const botaoModoClaro = document.getElementById('modo-claro');
-const body = document.body;
 
-botaoModoClaro.addEventListener('click', () => {
-  if (body.classList.contains('modo-claro')) {
-    body.classList.remove('modo-claro');
-    body.classList.add('modo-escuro');
-  } else {
-    body.classList.remove('modo-escuro');
-    body.classList.add('modo-claro');
-  }
-});
